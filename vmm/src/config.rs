a5bc6e362928fe0e19295b45682bac317a336ce0
Hao Xu <howeyxu@tencent.com>
2022-10-11 16:02:16 +0800

    virtiofsd: implement the arguments delivery and other tweak
     - now the arguments of virtiofsd is from CH's booting cmd
       we should change the way virtiofsd receives them
    
    Signed-off-by: Hao Xu <howeyxu@tencent.com>

--- vmm/src/config.rs
+++ vmm/src/config.rs
@@ -1616,6 +1616,7 @@ pub struct FsConfig {
     pub id: Option<String>,
     #[serde(default)]
     pub pci_segment: u16,
+    pub virtiofsd_args: String,
 }
 
 fn default_fsconfig_num_queues() -> usize {
@@ -1635,6 +1636,7 @@ impl Default for FsConfig {
             queue_size: default_fsconfig_queue_size(),
             id: None,
             pci_segment: 0,
+            virtiofsd_args: "".to_string(),
         }
     }
 }
@@ -1643,6 +1645,42 @@ impl FsConfig {
     pub const SYNTAX: &'static str = "virtio-fs parameters \
     \"tag=<tag_name>,socket=<socket_path>,num_queues=<number_of_queues>,\
     queue_size=<size_of_each_queue>,id=<device_id>,pci_segment=<segment_id>\"";
+
+    const VIRTIOFSD_ARGS: &'static [&'static str] = &[
+    "--shared-dir", "--thread-pool-size", "--xattr", "--posix-acl", "--xattrmap",
+    "--seccomp", "--announce-submounts", "--inode-file-handles",
+    "--cache", "--no-readdirplus", "--writeback", "--allow-direct-io", "--print-capabilities",
+    "--modcaps", "--log-level", "--rlimit-nofile", "--compat-options",
+    "--compat-debug", "--no-killpriv-v2", "--killpriv-v2", "--compat-foreground",
+    "--security-label", "--socket-path", "--sandbox", "--syslog",
+    ];
+
+
+    fn parser_add_virtiofsd(parser: &mut OptionParser) {
+        for option in FsConfig::VIRTIOFSD_ARGS {
+            parser.add(option);
+        }
+    }
+
+    fn parse_virtiofsd(parser: &OptionParser) -> String {
+        let mut virtiofsd_args = String::new();
+
+        for i in 0..FsConfig::VIRTIOFSD_ARGS.len() - 3 {
+            let key = FsConfig::VIRTIOFSD_ARGS[i];
+            if let Some(val) = parser.get(key) {
+                virtiofsd_args.push_str(&(key.to_string() + "=" + &val + ","));
+            };
+        }
+
+        let socket_path = "--socket-path".to_string() + "=" +
+                          &parser.get("socket").unwrap() + ",";
+        virtiofsd_args.push_str(&socket_path);
+
+        let sandbox_mode = "--sandbox".to_string() + "=none";
+        virtiofsd_args.push_str(&sandbox_mode);
+
+        virtiofsd_args
+    }
 
     pub fn parse(fs: &str) -> Result<Self> {
         let mut parser = OptionParser::new();
@@ -1653,6 +1691,8 @@ impl FsConfig {
             .add("socket")
             .add("id")
             .add("pci_segment");
+
+        Self::parser_add_virtiofsd(&mut parser);
         parser.parse(fs).map_err(Error::ParseFileSystem)?;
 
         let tag = parser.get("tag").ok_or(Error::ParseFsTagMissing)?;
@@ -1674,6 +1714,8 @@ impl FsConfig {
             .map_err(Error::ParseFileSystem)?
             .unwrap_or_default();
 
+        let virtiofsd_args = Self::parse_virtiofsd(&parser);
+
         Ok(FsConfig {
             tag,
             socket,
@@ -1681,6 +1723,7 @@ impl FsConfig {
             queue_size,
             id,
             pci_segment,
+            virtiofsd_args,
         })
     }
 
@@ -2422,6 +2465,7 @@ impl VmConfig {
         }
 
         if let Some(fses) = &self.fs {
+            // [Hao Xu]
             if !fses.is_empty() && !self.memory.shared {
                 return Err(ValidationError::VhostUserRequiresSharedMemory);
             }
