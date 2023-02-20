21d0e5fc0f9f067a989d5317a056d4593441be1d
Hao Xu <howeyxu@tencent.com>
2023-02-20 19:22:10 +0800

    virtio-devices: fs: add shared_dir logic
    shared_dir is the most important parameter of virtio-fs, which indicates
    the shared directory between guest os and host.
    
    Signed-off-by: Hao Xu <howeyxu@tencent.com>

--- virtio-devices/src/fs.rs
+++ virtio-devices/src/fs.rs
@@ -15,6 +15,7 @@ use crate::{
 use anyhow::anyhow;
 use seccompiler::SeccompAction;
 use serde::Deserialize;
+use std::fs;
 use std::io;
 use std::os::unix::io::AsRawFd;
 use std::result;
@@ -317,7 +318,14 @@ impl Fs {
     }
 
     fn init_backend_fs(&self, backendfs_config: &BackendFsConfig) -> io::Result<PassthroughFs> {
+        let shared_dir = &backendfs_config.shared_dir;
+        let shared_dir_rp = fs::canonicalize(shared_dir.to_string())?;
+        let shared_dir_rp_str = shared_dir_rp
+            .to_str()
+            .ok_or_else(|| io::Error::from_raw_os_error(libc::EINVAL))?;
+
         let fs_cfg = passthrough::Config {
+            root_dir: shared_dir_rp_str.into(),
             mountinfo_prefix: None,
             proc_sfd_rawfd: None,
             proc_mountinfo_rawfd: None,
